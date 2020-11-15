85b22f2176c952a82c0d138e3645ff7a3d1cf31d
Julian Brea <jbrea@baufest.com>
2020-11-15 18:56:28 -0300

    Commit inicial

---
+++ components/footer/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Gracias por utilizar nuestra plataforma!</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Ecommerce | Hecho con ❤ para <b> <a href="https://www.linkedin.com/in/johanna-avila7391/"  style="color: #FF33FF">Johanna Avila </a></b></p>
+    </div>
+      <div class="navbar-item social">
+            <a href="#" class="icon" :title="facebookTooltip">
+              <i class="fa fa-facebook"></i>
+            </a>
+            <a href="#" class="icon" :title="twitterTooltip">
+              <i class="fa fa-twitter"></i>
+            </a>
+            <a href="#" class="icon" :title="instagramTooltip">
+              <i class="fa fa-instagram"></i>
+            </a>
+            <a href="#" class="icon" :title="linkedinTooltip">
+              <i class="fa fa-linkedin"></i>
+            </a>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Ecommerce
+        </h1>
+        <h2 class="subtitle">
+          Aplicación desarrollada para AMS
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgb(131, 152, 165);
+    }
+    h1, h2 {
+      color: #ffffff;
+    }
+</style>
